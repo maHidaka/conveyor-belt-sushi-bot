d2b4cee9252942d1d8d21d34a19ad37549b3878f
MasatoHidaka <m.hdk816@gmail.com>
2022-06-06 15:38:45 +0900

    ラインセンサでサーボをコントロールするテスト

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
-#include <Arduino.h>
+#include <ESP32Servo.h>
+Servo servo_L;
+Servo servo_R;
 
 void setup()
 {
   pinMode(23, OUTPUT);
+  servo_L.attach(16);
+  servo_R.attach(17);
   Serial.begin(115200);
 }
 
@@ -10,13 +14,18 @@ void loop()
 {
   int val = analogRead(34);
   Serial.println(val);
+
   if (val < 3000)
   {
     digitalWrite(23, HIGH);
+    servo_L.write(0);
+    servo_R.write(0);
   }
   else
   {
     digitalWrite(23, LOW);
+    servo_L.write(180);
+    servo_R.write(180);
   }
   delay(10);
 }
